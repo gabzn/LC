2c64f2f9048e8496976745dfc030528907303c50
gabzn <58453421+gabzn@users.noreply.github.com>
2020-11-18 16:26:23 -0500

    Update Reverse Integer.java

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -6,7 +6,9 @@ class Solution
         
         while(x != 0)
         {
-            if(toReturn < Integer.MIN_VALUE/10 || toReturn > Integer.MAX_VALUE/10) return 0;
+            if(toReturn < Integer.MIN_VALUE/10 || toReturn > Integer.MAX_VALUE/10) return 0;  //Every iteration 'toReturn' is going to multiply by 10.
+                                                                         //We want to make sure that 'toReturn' doesn't get beyong the scope of Integer when it's multiply by 10 next time.
+                                                                         //We can check that by comparing the MIN and MAX values of Integer dividing by 10.                 
             toReturn = toReturn*10 + x%10;
             x = x/10;
         }
@@ -14,5 +16,4 @@ class Solution
         return toReturn;
     }
 }
-
 //Use % when dealing with reversing an integer.
