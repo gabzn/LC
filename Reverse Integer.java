b9ba038c4c8785bdcfdb8ca4bfa1a6a9518b6a83
gabzn <58453421+gabzn@users.noreply.github.com>
2020-11-18 13:35:54 -0500

    Update Reverse Integer.java

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -3,15 +3,16 @@ class Solution
     public int reverse(int x) 
     {
         int toReturn = 0;
-    
+        
         while(x != 0)
         {
-            if(Math.abs(toReturn) > Integer.MAX_VALUE / 10) return 0;
+            if(toReturn < Integer.MIN_VALUE/10 || toReturn > Integer.MAX_VALUE/10) return 0;
             toReturn = toReturn*10 + x%10;
             x = x/10;
         }
+        
         return toReturn;
     }
 }
 
-//Use % when dealing with problmes like reversing an integer.
+//Use % when dealing with reversing an integer.
