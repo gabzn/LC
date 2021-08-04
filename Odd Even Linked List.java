cb7059bce6befe66c1bdce3a4ca42aa57f230b3a
Gabriel <58453421+gabzn@users.noreply.github.com>
2021-08-03 22:49:31 -0400

    Update Odd Even Linked List.java

--- Odd Even Linked List.java
+++ Odd Even Linked List.java
@@ -1,9 +1,6 @@
 Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
-
 The first node is considered odd, and the second node is even, and so on.
-
 Note that the relative order inside both the even and odd groups should remain as it was in the input.
-
 You must solve the problem in O(1) extra space complexity and O(n) time complexity.
   
 Input: head = [1,2,3,4,5]
@@ -16,9 +13,9 @@ class Solution
 {
     public ListNode oddEvenList(ListNode head) 
     {
-        // Edge cases
+        // Edge cases. The second check is not necessary, but it helped me to solve the problem in the first place.
         if(head == null) return null;
-        if(head.next == null) return head;
+        // if(head.next == null) return head; 
         
         // oddIndex and evenIndex will move through the list. 
         // evenHead stays at the second node.
