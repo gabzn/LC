182f292929f5958b9fc49e0e317fc1c110b2a611
Gabriel <58453421+gabzn@users.noreply.github.com>
2021-08-18 00:00:59 -0400

    Update Add Binary.java

--- Add Binary.java
+++ Add Binary.java
@@ -1,3 +1,8 @@
+Given two binary strings a and b, return their sum as a binary string.
+    
+Input: a = "1010", b = "1011"
+Output: "10101"   
+
 class Solution 
 {
     public String addBinary(String a, String b) 
@@ -9,27 +14,22 @@ class Solution
         
       while(al >= 0 && bl >= 0)
       {
-          # Key thing to learn is how to convert a char to an int.
+          // Key thing to learn is how to convert char to int.
           int sum = (a.charAt(al) - '0') + (b.charAt(bl) - '0') + carry;
           
-          # sum can either be 2 or 3 if this condition is true.
           if(sum >= 2)
           {
               carry = 1;
               sum = sum % 2;
-              sb.insert(0,sum);
           }
-          else
-          {
-              carry = 0;
-              sb.insert(0,sum);
-          }
+          else    carry = 0;
           
+          sb.insert(0,sum);
           al--;
           bl--;
       }
        
-       # Cases where one string is longer than the other.
+       // These two for loops take care of the case where one string is longer than the other.
        while(al >= 0)
        {
            if( (a.charAt(al) - '0') + carry == 2)
@@ -60,8 +60,7 @@ class Solution
            bl--;
        }
        
-       # Final case where the leftmost number also gives a carry, if it gives a carry, append a '1' to the very beginning of the string.
        if(carry == 1) sb.insert(0,1);
-       return sb.toString();
+       return sb.toString();    
     }
 }
