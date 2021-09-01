d691df6f8a199e65c7859cf1c42574011e909995
Gabriel <58453421+gabzn@users.noreply.github.com>
2021-09-01 17:57:26 -0400

    Update Max Subarray.java

--- Max Subarray.java
+++ Max Subarray.java
@@ -4,15 +4,15 @@ class Solution
     {
         if(nums.length == 1) return nums[0];
         
-        //Assume the first element is the max.
+        // Assume the first element is the max.
         int maxSum = nums[0];
         int currentSum = nums[0];
         
-        //Loop starts at the secondg element.
+        // Loop starts at the second element.
         for(int i=1;i<nums.length;i++)
         {
-            //If nums[i] itself is greater than the sum of all previous numbers,
-            //that means we'll be better off dropping all previous numbers, and use nums[i] as our new starting point.
+            // If nums[i] itself is greater than the sum of all previous numbers,
+            // that means we'll be better off dropping all previous numbers, and use nums[i] as our new starting point.
             if(nums[i] > (currentSum + nums[i])) currentSum = nums[i];
             else currentSum = currentSum + nums[i];
             
