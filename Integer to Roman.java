6880c22259dc28f68405a7e89edbc460e20f98cb
Gabriel <58453421+gabzn@users.noreply.github.com>
2021-12-29 00:19:37 -0500

    Create Integer to Roman.java

---
+++ Integer to Roman.java
@@ -0,0 +1,33 @@
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+  
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+  
+  
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+                
+        int[] digits = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        while(num != 0) {
+            
+            for(int i=0;i<digits.length;i++){
+                
+                if(num >= digits[i]) {
+                    String value = romans[i];
+                    sb.append(value);
+                    num = num - digits[i];
+                    break;
+                }
+                
+            }
+        }
+        
+        return sb.toString();
+    }
+}
