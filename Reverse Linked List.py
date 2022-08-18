76a16e0138b73ab7dd089814557366dfe0436ae0
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-08-18 01:16:41 -0400

    Update Reverse Linked List.py

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -20,3 +20,15 @@ class Solution:
         
         new_head.next = prev
         return new_head
+
+    
+ class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        
+        return new_head
