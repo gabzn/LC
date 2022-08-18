65bcd35321a31a9666afef7eaa97ad8bce0ff783
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-08-17 23:42:12 -0400

    Update Merge Two Sorted Lists.java

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -9,66 +9,42 @@
  * }
  */
  
-class Solution 
-{
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) 
-    {
-        if(l1==null && l2==null) return null;
-        if(l1==null) return l2;
-        if(l2==null) return l1;
-            
-        ListNode toReturn;
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null && list2 == null) return null;
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
         
-        //Find the first smallest value in the given lists.
-        if(l1.val<=l2.val)
-        {
-            toReturn = new ListNode(l1.val);
-            l1 = l1.next;
-        }
-        else
-        {
-            toReturn = new ListNode(l2.val);
-            l2 = l2.next;
+        // Find the first smallest node between both lists.
+        ListNode newHead;
+        if(list1.val <= list2.val) {
+            newHead = new ListNode(list1.val);
+            list1 = list1.next;
+        } else {
+            newHead = new ListNode(list2.val);
+            list2 = list2.next;
         }
         
-        //Initialize a pointer. ptr will link all other nodes in order.
-        ListNode ptr = toReturn;
-        
-        //As long as neither of them is null, keep adding the smaller one to toReturn.
-        //After adding a node to toReturn, remove that node from the list.
-        while(l1 != null && l2 != null)
-        {
-            if(l1.val<=l2.val)
-            {
-                ptr.next = new ListNode(l1.val);
-                l1 = l1.next;
-            }
-            else
-            {
-                ptr.next = new ListNode(l2.val);
-                l2 = l2.next;
+        // Use ptr to connect all the remaining nodes.
+        // Loop through both lists and compare both vals
+        ListNode ptr = newHead;
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                ptr.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                ptr.next = new ListNode(list2.val);
+                list2 = list2.next;
             }
             ptr = ptr.next;
         }
         
-//         while(l1 != null)
-//         {
-//             temp.next = new ListNode(l1.val);
-//             temp = temp.next;
-//             l1 = l1.next;
-//         }
+        if(list1 == null) {
+            ptr.next = list2;
+        } else {
+            ptr.next = list1;
+        }
         
-//         while(l2 != null)
-//         {
-//             temp.next = new ListNode(l2.val);
-//             temp = temp.next;
-//             l2 = l2.next;
-//         }
-     
-        //If either one of the lists is empty, temp could just point to the non-empty list since the non-empty list is sorted already.
-        if(l1 == null)    ptr.next = l2;
-        else     ptr.next = l1;
-      
-        return toReturn;  
+        return newHead;
     }
 }
