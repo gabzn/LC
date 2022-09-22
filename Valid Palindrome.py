b25dcd7af2af7005cde49c109de68a624441c4b2
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-09-21 22:33:52 -0400

    Update Valid Palindrome.py

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -32,3 +32,26 @@ class Solution:
             r -= 1
         
         return True
+      
+------------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True
+        
+        l, r = 0, len(s) - 1
+        while l <= r:
+            
+            # Move both l and r if the current characters 
+            # are not alphanumeric.
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+        return True
