fa48f39301993f3aef0a08dc09724d312dcd4f29
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-11-11 23:43:50 -0500

    Update Meeting Rooms II.py

--- Meeting Rooms II.py
+++ Meeting Rooms II.py
@@ -1,3 +1,25 @@
+https://leetcode.com/problems/meeting-rooms-ii/
+      
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        total_rooms, current_rooms_needed = 0, 0
+        room_allocations = []
+        
+        intervals.sort(key=lambda interval: interval[0])
+        for interval in intervals:
+            start, end = interval
+            room_allocations.append([start, 1])
+            room_allocations.append([end, -1])
+        
+        room_allocations.sort(key=lambda allocation: allocation[0])
+        for allocation in room_allocations:
+            current_rooms_needed += allocation[1]
+            total_rooms = max(total_rooms, current_rooms_needed)
+            
+        return total_rooms
+
+-------------------------------------------------------------------------------------------------------------------------
+
 Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
 find the minimum number of conference rooms required.
 
