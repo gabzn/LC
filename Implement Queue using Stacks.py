1370fc462f0167dad30712fd269ff7cd5994da82
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-11-20 16:51:37 -0500

    Update Implement Queue using Stacks.py

--- Implement Queue using Stacks.py
+++ Implement Queue using Stacks.py
@@ -1,5 +1,33 @@
 https://leetcode.com/problems/implement-queue-using-stacks/
-  
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+        self.placeholder = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        
+        while len(self.stack) > 1:
+            val = self.stack.pop()
+            self.placeholder.append(val)
+        
+        res = self.stack.pop()
+        
+        while self.placeholder:
+            self.push(self.placeholder.pop())
+        
+        return res
+            
+    def peek(self) -> int:
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0  
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
 class MyQueue:
 
     def __init__(self):
