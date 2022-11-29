61001fe7c685e179f458701c74ed6e0379cc1369
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-11-29 18:11:03 -0500

    Update Validate Binary Search Tree.py

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -2,8 +2,6 @@ https://leetcode.com/problems/validate-binary-search-tree/
   
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
         return self.validate(root, -math.inf, math.inf)
         
     # The boundaries will be adjusted depends on whether we are checking the left subtree or the right subtree. 
@@ -21,3 +19,18 @@ class Solution:
         if root.val <= min_boundary or root.val >= max_boundary:
             return False
         return self.validate(root.left, min_boundary, root.val) and self.validate(root.right, root.val, max_boundary)
+      
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.check_if_subtrees_are_valid(root, -math.inf, math.inf)
+        
+    def check_if_subtrees_are_valid(self, root, lower_bound, upper_bound):
+        if not root:
+            return True
+        
+        # Check if the current node val is within the bound.
+        if not lower_bound < root.val < upper_bound:
+            return False
+        
+        return self.check_if_subtrees_are_valid(root.left, lower_bound, root.val) and self.check_if_subtrees_are_valid(root.right, root.val, upper_bound)
