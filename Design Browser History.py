f2488b9f436cfc02c4b789a59fe90f7d4182ed52
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-11-30 00:21:48 -0500

    Update Design Browser History.py

--- Design Browser History.py
+++ Design Browser History.py
@@ -1,5 +1,45 @@
 https://leetcode.com/problems/design-browser-history/
-  
+
+class Node:
+    def __init__(self, url=None, prev=None, next=None):
+        self.url = url
+        self.prev = prev
+        self.next = next
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.head = Node()
+        self.tail = Node()
+        self.cur = Node(homepage)
+        self.insert(self.head, self.cur, self.tail)
+
+    def insert(self, prev, cur, tail):
+        prev.next = cur
+        cur.prev = prev
+        
+        tail.prev = cur
+        cur.next = tail
+            
+    def visit(self, url: str) -> None:
+        new_node = Node(url)
+        self.insert(self.cur, new_node, self.tail)
+        self.cur = new_node
+        
+    def back(self, steps: int) -> str:
+        while self.cur.prev != self.head and steps:
+            self.cur = self.cur.prev
+            steps -= 1
+            
+        return self.cur.url
+
+    def forward(self, steps: int) -> str:
+        while self.cur.next != self.tail and steps:
+            self.cur = self.cur.next
+            steps -= 1
+        
+        return self.cur.url
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
 class BrowserHistory:
 
     def __init__(self, homepage: str):
