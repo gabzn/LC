ebae561853f423ab112887bb06fa924c912431de
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-12-18 17:38:33 -0500

    Create Range Sum Query - Immutable.py

---
+++ Range Sum Query - Immutable.py
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/range-sum-query-immutable/
+  
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.prefix_sum_nums = self.build_prefix_sum_nums(self.nums)
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.prefix_sum_nums[right]
+        
+        return self.prefix_sum_nums[right] - self.prefix_sum_nums[left-1]
+
+    def build_prefix_sum_nums(self, nums):
+        prefix_sum_nums = [nums[0]]
+        
+        for num in nums[1:]:
+            prefix_sum_nums.append(prefix_sum_nums[-1] + num)
+                
+        return prefix_sum_nums
