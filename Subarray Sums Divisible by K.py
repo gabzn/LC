f11204f6df2063c4b61fd66805b3762b810adf99
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-12-22 15:56:21 -0500

    Update Subarray Sums Divisible by K.py

--- Subarray Sums Divisible by K.py
+++ Subarray Sums Divisible by K.py
@@ -1,6 +1,23 @@
 https://leetcode.com/problems/subarray-sums-divisible-by-k/
 https://www.youtube.com/watch?v=iABMUwDN6L0
-  
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        remainder_counts = defaultdict(int)
+        remainder_counts[0] += 1
+        running_sum, ans = 0, 0
+        
+        for num in nums:
+            running_sum += num
+            
+            remainder = running_sum % k
+            if remainder in remainder_counts:
+                ans += remainder_counts[remainder]
+            
+            remainder_counts[remainder] += 1
+
+        return ans
+-----------------------------------------------------------------------------------------------  
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         remainder_counts = {0: 1}
