1f9d3e428c480f8cfa50fab560b837dae6ad5d47
Gabriel <58453421+gabzn@users.noreply.github.com>
2022-12-23 21:52:33 -0500

    Create Encode and Decode Strings.py

---
+++ Encode and Decode Strings.py
@@ -0,0 +1,41 @@
+https://leetcode.com/problems/encode-and-decode-strings/
+  
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        encoded_str = ''
+        """
+        Get the length of each str and encode it with its length on the front
+        Separate its length and the str with a ! to indicate its length .
+        
+        [Hello, World] -> 5!Hello5!World
+        """
+        for s in strs:
+            LEN = len(s)
+            encoded_str = encoded_str + str(LEN) + '!' + s
+        
+        return encoded_str
+
+    def decode(self, s: str) -> List[str]:
+        decoded_strs = []
+        pointer, LEN = 0, len(s)
+        
+        while pointer < LEN:
+            cur_len, cur_str = '', ''
+            seen_delimeter = False
+            
+            while not seen_delimeter:
+                cur_char = s[pointer]
+                if cur_char == '!':
+                    seen_delimeter = True
+                else:
+                    cur_len = cur_len + cur_char
+                pointer += 1
+                 
+            cur_len = int(cur_len)
+            while cur_len:
+                cur_str = cur_str + s[pointer]
+                cur_len -= 1
+                pointer += 1
+            
+            decoded_strs.append(cur_str)
+        return decoded_strs
