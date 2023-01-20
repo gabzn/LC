9a009aa80b85c7cbdf2771d0dd453c6168613150
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-01-19 20:35:00 -0500

    Update Word Search.py

--- Word Search.py
+++ Word Search.py
@@ -1,5 +1,37 @@
 https://leetcode.com/problems/word-search/
-  
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        R, C = len(board), len(board[0])
+        
+        for r in range(R):
+            for c in range(C):
+                if self.word_found(board, R, C, r, c, word, 0):
+                    return True
+                
+        return False
+    
+    def word_found(self, board, R, C, r, c, word, idx):
+        if  board[r][c] != word[idx]:
+            return False
+        
+        if idx == len(word) - 1:
+            return True
+        
+        neighbours = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+        for neighbour_r, neighbour_c in neighbours:
+            if 0 <= neighbour_r < R and 0 <= neighbour_c < C:
+                
+                letter = board[r][c]
+                board[r][c] = '-1'
+                
+                if self.word_found(board, R, C, neighbour_r, neighbour_c, word, idx + 1):
+                    return True
+                
+                board[r][c] = letter 
+            
+        return False
+---------------------------------------------------------------------------------------------------------------------------------------------------------
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def dfs(r, c, ind):
