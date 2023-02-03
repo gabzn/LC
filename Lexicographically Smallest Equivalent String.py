9bdad278ca7f672831b7662e248161e26b836b2f
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-02-02 23:29:48 -0500

    Update Lexicographically Smallest Equivalent String.py

--- Lexicographically Smallest Equivalent String.py
+++ Lexicographically Smallest Equivalent String.py
@@ -10,20 +10,23 @@ class Solution:
         
         def union(x, y):
             root_x, root_y = find(x), find(y)
+            if root_x == root_y:
+                return 
             
             if root_x < root_y:
                 roots[root_y] = root_x 
             else:
                 roots[root_x] = root_y
             
+        
         roots = [i for i in range(26)]
-        for a, b in zip(s1, s2):
-            a_num, b_num = ord(a) - ord('a'), ord(b) - ord('a')
-            union(a_num, b_num)
-
+        for x, y in zip(s1, s2):
+            union(ord(x) - 97, ord(y) - 97)
+            # union(ord(x) - ord('a'), ord(y) - ord('a'))
+            
         res = ''
         for char in baseStr:
-            root = find(ord(char) - ord('a'))
-            res += chr(root + ord('a'))
+            root = find(ord(char) - 97)
+            res += chr(root + 97)
  
         return res
