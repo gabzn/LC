95c7da9d55b05d57ddb4e1027abecbf62c9a6fe5
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-02-16 23:52:42 -0500

    Create Minimum Absolute Difference in BST.py

---
+++ Minimum Absolute Difference in BST.py
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+  
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        preorder = []
+        self.get_preorder(root, preorder)
+        
+        min_difference = math.inf
+        for i in range(1, len(preorder)):
+            min_difference = min(min_difference, preorder[i] - preorder[i - 1])
+        
+        return min_difference
+    
+    def get_preorder(self, root, preorder):
+        if not root:
+            return
+        
+        self.get_preorder(root.left, preorder)
+        preorder.append(root.val)
+        self.get_preorder(root.right, preorder)    
