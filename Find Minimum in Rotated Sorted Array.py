9cdfe84974f93bb1eae19268494aae7967e2b7ac
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-02-27 22:53:38 -0500

    Update Find Minimum in Rotated Sorted Array.py

--- Find Minimum in Rotated Sorted Array.py
+++ Find Minimum in Rotated Sorted Array.py
@@ -1,16 +1,21 @@
-Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
-    [4,5,6,7,0,1,2] if it was rotated 4 times.
-    [0,1,2,4,5,6,7] if it was rotated 7 times.
-    
-    
-Input: nums = [3,4,5,1,2]
-Output: 1
-Explanation: The original array was [1,2,3,4,5] rotated 3 times.
-  
-Input: nums = [4,5,6,7,0,1,2]
-Output: 0
-Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
-  
+https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        res = math.inf
+        
+        while l <= r:
+            m = (l + r) // 2
+            
+            res = min(res, nums[l], nums[m], nums[r])
+            if nums[m] < nums[r]:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return res    
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------    
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         if len(nums) == 1:
