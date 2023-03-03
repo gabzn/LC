40e082462077ad7b8568acdacdb63e0ae2177bf3
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-03-02 22:38:32 -0500

    Update Find Peak Element.py

--- Find Peak Element.py
+++ Find Peak Element.py
@@ -7,6 +7,8 @@ class Solution:
         while l < r:
             m = (l + r) // 2
             
+            # The constraints say i != i + 1, so we don't need to care about when they are equal.
+            # If the middle > the element to its right, the peak can only be on the left side or the middle.
             if nums[m] > nums[m + 1]:
                 r = m
             else:
