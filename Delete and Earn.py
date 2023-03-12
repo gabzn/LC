c82813c077de09db5e06bbd7cffaf41b8992e2b0
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-03-11 23:41:13 -0500

    Update Delete and Earn.py

--- Delete and Earn.py
+++ Delete and Earn.py
@@ -19,3 +19,20 @@ class Solution:
             memo[i] = max(points[i] + self.dp(points, i - 2, memo), self.dp(points, i - 1, memo))
         
         return memo[i]
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        max_num, points = 0, collections.defaultdict(int)
+        
+        for num in nums:
+            points[num] += num
+            max_num = max(max_num, num)
+        
+        # dp[i] tells us the max pts we can get picking the i num
+        dp = [0] * (max_num + 1)
+        dp[1] = points[1]      # A bit tricky, but if there's no 1 the default is 0
+        
+        for i in range(2, max_num + 1):
+            dp[i] = max(dp[i - 2] + points[i], dp[i - 1])
+        
+        return dp[-1]
