9a90b788428d430c14dbbd8a43285812a104ab1d
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-03-15 15:40:37 -0400

    Update Longest Common Subsequence.py

--- Longest Common Subsequence.py
+++ Longest Common Subsequence.py
@@ -18,3 +18,17 @@ class Solution:
             return memo[(l, r)]
         
         return dp(0, 0, {})
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        LEN1, LEN2 = len(text1), len(text2)
+        dp = [[0 for j in range(LEN2 + 1)] for i in range(LEN1 + 1)]
+        
+        for i in range(0, LEN1):
+            for j in range(0, LEN2):
+                if text1[i] == text2[j]:
+                    dp[i + 1][j + 1] = 1 + dp[i][j]
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+        
+        return dp[-1][-1]
