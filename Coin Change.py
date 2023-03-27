53411995d52735f2e4c6441f06682eb04636e177
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-03-26 23:12:01 -0400

    Update Coin Change.py

--- Coin Change.py
+++ Coin Change.py
@@ -1,28 +1,39 @@
 https://leetcode.com/problems/coin-change/
+
   
-You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
-
-Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
-
-You may assume that you have an infinite number of each kind of coin.
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [math.inf] * (amount + 1)
+        # dp(cur_amount) returns the min number of coins to make up cur_amount
+        def dp(cur_amount, memo):
+            if cur_amount == 0:
+                return 0
+            
+            if cur_amount not in memo:
+                res = math.inf
+                
+                for coin in coins:
+                    if cur_amount == coin:
+                        res = 1
+                    elif cur_amount > coin:
+                        res = min(res, 1 + dp(cur_amount - coin, memo))
+                    
+                memo[cur_amount] = res
+                
+            return memo[cur_amount]
+        
+        res = dp(amount, {})
+        return res if res != math.inf else -1  
+-------------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:        
+        dp = [math.inf for _ in range(amount + 1)]
         dp[0] = 0
         
-        """
-        the size of dp has to be (amount + 1) is because we want to know the number of coins to change from 0 up to amount.
-        dp[x] = # of coins it needs to change the amount of x.
-        if x - coin is equal to 0, that means x can be changed exactly using 1 of this coin.
-        if x - coin is greater than 0, that means after we use coin to change x there's still some something we can change.
-        """
-        
-        for x in range(1, amount + 1):
+        for i in range(1, amount + 1):
             for coin in coins:
-                if x - coin == 0:
-                    dp[x] = 1
-                if x - coin > 0:
-                    dp[x] = min(dp[x], 1 + dp[x-coin])
+                if i == coin:
+                    dp[i] = 1
+                elif i > coin:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
         
-        return dp[amount] if dp[amount] != math.inf else -1
+        return dp[-1] if dp[-1] != math.inf else -1
