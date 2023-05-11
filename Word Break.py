76894095cc4bfc083f92ba62a609db27a2944f56
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-05-10 22:26:49 -0400

    Update Word Break.py

--- Word Break.py
+++ Word Break.py
@@ -1,23 +1,25 @@
 https://leetcode.com/problems/word-break/
   
 class Solution:
-    def wordBreak(self, s: str, words: List[str]) -> bool:
+    def wordBreak(self, s: str, word_dict: List[str]) -> bool:
         LEN = len(s)
-        words = set(words)
+        word_dict = set(word_dict)
         
         def dp(start, memo):
             if start == LEN:
                 return True
-            
             if start in memo:
                 return memo[start]
             
             memo[start] = False
             
-            for end in range(start + 1, LEN + 1):
-                if s[start: end] in words and dp(end, memo):
+            for end in range(start, LEN + 1):
+                # s[start: end] means the substring from start up to end - 1
+                # to include end, we need to do s[start: end + 1]
+                if s[start: end + 1] in word_dict and dp(end + 1, memo):
                     memo[start] = True
                     break
-                    
-            return memo[start]   
+            
+            return memo[start]
+            
         return dp(0, {})
