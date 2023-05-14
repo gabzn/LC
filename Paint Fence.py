84b7beab9504e12aa47e665cd15e6a73fcd1ccee
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-05-13 21:35:41 -0400

    Update Paint Fence.py

--- Paint Fence.py
+++ Paint Fence.py
@@ -18,3 +18,19 @@ class Solution:
             return memo[posts]
             
         return dp(n, {})
+-------------------------------------------------------------------------------------------------
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        if n == 1:
+            return k
+        if n == 2:
+            return k * k
+        
+        dp = [0] * (n + 1)
+        dp[1] = k
+        dp[2] = k * k
+        
+        for post in range(3, n + 1):
+            dp[post] = (k - 1) * dp[post - 1] + (k - 1) * dp[post - 2]
+        
+        return dp[n]
