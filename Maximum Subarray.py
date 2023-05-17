3304de11ddaa9eef099d467a3553a00897c7772d
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-05-17 00:14:10 -0400

    Update Maximum Subarray.py

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -6,8 +6,23 @@ class Solution:
         
         for num in nums:
             # If adding num to the running_sum makes it smaller
-            # that means it's better to take num and start fresh
+            # that means it's better to start fresh at num
             running_sum = max(running_sum + num, num)
             ans = max(running_sum, ans)
             
         return ans  
+      
+------------------------------------------------------------------------------------------------------
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        LEN = len(nums)
+        dp = [0] * LEN
+        dp[0] = nums[0]
+
+        prev_max = nums[0]
+
+        for idx in range(1, LEN):
+            prev_max = max(prev_max + nums[idx], nums[idx])
+            dp[idx] = prev_max
+        
+        return max(dp)    
