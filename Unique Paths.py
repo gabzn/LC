079f7c6952f64eb9ce91f476cd6431cae51f0a76
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-05-20 23:15:48 -0400

    Update Unique Paths.py

--- Unique Paths.py
+++ Unique Paths.py
@@ -15,3 +15,19 @@ class Solution:
             return memo[(x, y)]
             
         return dp(m - 1, n - 1, {})  
+-----------------------------------------------------------------------------------
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = 1
+        
+        for x in range(m):
+            for y in range(n):
+                if x > 0 and y > 0:
+                    dp[x][y] = dp[x][y - 1] + dp[x - 1][y]
+                if x == 0 and y > 0:
+                    dp[x][y] = dp[x][y - 1]
+                if y == 0 and x > 0:
+                    dp[x][y] = dp[x - 1][y]
+                    
+        return dp[-1][-1]
