e6847512e8e844001fbf20663ff0a1241c088ddc
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-05-22 23:36:36 -0400

    Update Unique Paths II.py

--- Unique Paths II.py
+++ Unique Paths II.py
@@ -18,3 +18,25 @@ class Solution:
             return memo[(x, y)]
             
         return dp(len(obstacleGrid) - 1, len(obstacleGrid[0]) - 1, {})    
+--------------------------------------------------------------------------------------------------------
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
+            return 0
+        
+        X, Y = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0 for _ in range(Y)] for _ in range(X)]
+        dp[0][0] = 1
+        
+        for x in range(X):
+            for y in range(Y):
+                if x > 0 and y > 0:
+                    dp[x][y] = 0 if obstacleGrid[x][y] == 1 else dp[x - 1][y] + dp[x][y - 1]
+                
+                if x > 0 and y == 0:
+                    dp[x][y] = 0 if obstacleGrid[x][y] == 1 else dp[x - 1][y]
+                        
+                if y > 0 and x == 0:
+                    dp[x][y] = 0 if obstacleGrid[x][y] == 1 else dp[x][y - 1]
+                
+        return dp[-1][-1]
