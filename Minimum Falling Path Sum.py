a8ddd0118f653f29dfdf8d21db27ddb0128252db
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-05-26 22:34:57 -0400

    Update Minimum Falling Path Sum.py

--- Minimum Falling Path Sum.py
+++ Minimum Falling Path Sum.py
@@ -19,3 +19,34 @@ class Solution:
             res = min(res, dp(0, c, memo))
             
         return res
+--------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        N = len(matrix)
+        dp = [[matrix[r][c] for c in range(N)] for r in range(N)]
+        
+        for r in range(1, N):
+            for c in range(N):
+                if 0 < c < N - 1:
+                    dp[r][c] = matrix[r][c] + min(dp[r - 1][c], dp[r - 1][c + 1], dp[r - 1][c - 1])
+                if c == 0:
+                    dp[r][c] = matrix[r][c] + min(dp[r - 1][c], dp[r - 1][c + 1])
+                if c == N - 1:
+                    dp[r][c] = matrix[r][c] + min(dp[r - 1][c], dp[r - 1][c - 1])
+         
+        return min(dp[-1])
+--------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        N = len(matrix)
+        
+        for r in range(1, N):
+            for c in range(N):
+                if 0 < c < N - 1:
+                    matrix[r][c] = matrix[r][c] + min(matrix[r - 1][c], matrix[r - 1][c + 1], matrix[r - 1][c - 1])
+                if c == 0:
+                    matrix[r][c] = matrix[r][c] + min(matrix[r - 1][c], matrix[r - 1][c + 1])
+                if c == N - 1:
+                    matrix[r][c] = matrix[r][c] + min(matrix[r - 1][c], matrix[r - 1][c - 1])
+                               
+        return min(matrix[-1])
