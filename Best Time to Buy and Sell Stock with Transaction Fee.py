0b35cd59cb428aa2d52c9428287bdc0065567a7b
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-05-29 23:26:08 -0400

    Update Best Time to Buy and Sell Stock with Transaction Fee.py

--- Best Time to Buy and Sell Stock with Transaction Fee.py
+++ Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -28,3 +28,15 @@ class Solution:
             
             return memo[(i, holding)]
         return dp(0, False, {})
+--------------------------------------------------------------------------------------------------------------
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        LEN = len(prices)
+        dp = [[0, 0] for _ in range(LEN)]
+        dp[0][1] = -prices[0]
+        
+        for i in range(1, LEN):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])        
+        
+        return max(dp[-1][0], dp[-1][1])
