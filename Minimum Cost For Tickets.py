f8d730d53fc8cbf2acd96daf61989bf141235c52
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-06-08 23:28:45 -0400

    Update Minimum Cost For Tickets.py

--- Minimum Cost For Tickets.py
+++ Minimum Cost For Tickets.py
@@ -25,3 +25,17 @@ class Solution:
             return memo[day]
             
         return dp(last_day, {})
+------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        last_day = days[-1]
+        dp = [0 for _ in range(last_day + 1)]
+        days = set(days)
+        
+        for day in range(1, last_day + 1):
+            if day in days:
+                dp[day] = min(dp[max(day-1, 0)] + costs[0], dp[max(day-7, 0)] + costs[1], dp[max(day-30, 0)] + costs[2])    
+            else:
+                dp[day] = dp[day - 1]
+        
+        return dp[-1]
