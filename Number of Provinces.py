cfb41127e19cac51a8751e69fa40fa3070e4f595
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-06-16 22:17:39 -0400

    Update Number of Provinces.py

--- Number of Provinces.py
+++ Number of Provinces.py
@@ -1,42 +1,36 @@
+https://leetcode.com/problems/number-of-provinces/
+
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:        
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
         def find(x):
-            if x == root[x]:
-                return x
-
-            root[x] = find(root[x])
+            if x != root[x]:
+                root[x] = find(root[x])
             return root[x]
-
+        
         def union(x, y):
             root_x, root_y = find(x), find(y)
-            
-            if root_x != root_y:
-                if rank[root_x] > rank[root_y]:
-                    root[root_y] = root_x
-                elif rank[root_x] < rank[root_y]:
-                    root[root_x] = root_y
-                else:
-                    root[root_y] = root_x
-                    rank[root_x] += 1
-                
-                provinces[0] -= 1
+            if root_x == root_y:
+                return
+        
+            provinces[0] -= 1
+        
+            if rank[root_x] > rank[root_y]:
+                root[root_y] = root_x
+            elif rank[root_x] < rank[root_y]:
+                root[root_x] = root_y
+            else:
+                root[root_y] = root_x
+                rank[root_x] += 1
         
         N = len(isConnected)
         provinces = [N]
-
-        root = [i for i in range(N + 1)]
-        rank = [1] * (N + 1)
+        root = [i for i in range(N)]
+        rank = [1 for _ in range(N)]
         
-        for idx, connection in enumerate(isConnected):
-            for i, city in enumerate(connection):
-                if city == 1:
-                    union(idx + 1, i + 1)
-        
-        # # Count the number of root nodes that are equal to themselves.         
-        # res = -1
-        # for idx, r in enumerate(root):
-        #     if idx == r:
-        #         res += 1
-        # return res
-        
+        for i, cities in enumerate(isConnected):
+            for j, city in enumerate(cities):
+                if i == j or city == 0:
+                    continue
+                union(i, j)
+                
         return provinces[0]
