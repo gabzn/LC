4e0c82e7e514ac5374e9fa5fe6442266f0af786c
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-06-18 00:12:07 -0400

    Update The Earliest Moment When Everyone Become Friends.py

--- The Earliest Moment When Everyone Become Friends.py
+++ The Earliest Moment When Everyone Become Friends.py
@@ -3,38 +3,26 @@ https://leetcode.com/problems/the-earliest-moment-when-everyone-become-friends/
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
         def find(x):
-            if x == root[x]:
-                return x
-            root[x] = find(root[x])
+            if x != root[x]:
+                root[x] = find(root[x])
             return root[x]
         
-        def union(x, y, timestamp):
+        def union(x, y):
             root_x, root_y = find(x), find(y)
-            if root_x != root_y:
-                if rank[root_x] > rank[root_y]:
-                    root[root_y] = root_x
-                elif rank[root_x] < rank[root_y]:
-                    root[root_x] = root_y                
-                else:
-                    root[root_y] = root_x
-                    rank[root_x] += 1
-                
-                components[0] -= 1
-                if components[0] == 1:
-                    time[0] = timestamp
-                    return True
+            if root_x == root_y:
                 return False
+            
+            root[root_y] = root_x
+            return True
         
         logs.sort(key=lambda log: log[0])
-        
         root = [i for i in range(n)]
-        rank = [1 for _ in range(n)]
-        
-        # If the number of components can be reduced to 1, that means they can all be friends. Thus, a solution exists.
-        time, components = [math.inf], [n]
-        
-        for t, a, b in logs:
-            if union(a, b, t):
-                return time[0]
-        
+        groups = n
+                
+        for timestamp, x, y in logs:
+            if union(x, y):
+                groups -= 1
+            if groups == 1:
+                return timestamp
+                
         return -1
