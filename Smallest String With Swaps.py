148e28b73de677fb9bb6473e8863365a0aff1fcf
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-06-19 00:18:24 -0400

    Update Smallest String With Swaps.py

--- Smallest String With Swaps.py
+++ Smallest String With Swaps.py
@@ -1,5 +1,46 @@
 https://leetcode.com/problems/smallest-string-with-swaps/
-  
+
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        def find(x):
+            if x != root[x]:
+                root[x] = find(root[x])
+            return root[x]
+        
+        def union(x, y):
+            root_x, root_y = find(x), find(y)
+            if root_x == root_y:
+                return
+            root[root_y] = root_x
+        
+        LEN = len(s)
+        root = [i for i in range(LEN)]
+        res = ['' for _ in range(LEN)] 
+        indices_in_each_set = collections.defaultdict(list)
+        
+        # Union all the pairs
+        for a, b in pairs:
+            union(a, b)
+        
+        # Adjust the root and put indices that are in the same set in a list
+        for idx in range(LEN):
+            root_idx = find(idx)
+            indices_in_each_set[root_idx].append(idx)
+        
+        for _, indices in indices_in_each_set.items():
+            
+            # Get the chars in the same set and sort them to get the smallest str in each set
+            smallest_str_in_each_set = []
+            for index in indices:
+                smallest_str_in_each_set.append(s[index])
+            smallest_str_in_each_set.sort()
+            
+            # Now we have the smallest str in each set, we put them back according to the indices
+            for char, index in zip(smallest_str_in_each_set, indices):
+                res[index] = char
+        
+        return ''.join(res)
+--------------------------------------------------------------------------------------------------------
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         def find(x):
