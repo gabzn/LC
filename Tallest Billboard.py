9f563c166f48cb984f468c1d52ff7c5b7cbf89ce
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-06-25 01:05:05 -0400

    Update Tallest Billboard.py

--- Tallest Billboard.py
+++ Tallest Billboard.py
@@ -1,16 +1,18 @@
 https://leetcode.com/problems/tallest-billboard/
+https://leetcode.com/problems/tallest-billboard/discuss/3676384/Brute-to-Memoization-Total-Explanation
 
 class Solution:
     def tallestBillboard(self, rods: List[int]) -> int:
-        # dp(i) returns the tallest billboard we can install at index i when the height difference is difference
+        
+        # We want the difference to be 0
         def dp(i, difference, memo):
             if i == len(rods):
                 return 0 if difference == 0 else -math.inf
             if (i, difference) in memo:
                 return memo[(i, difference)]
             
-            memo[(i, difference)] = max(dp(i + 1, difference, memo), \
-                                        dp(i + 1, difference + rods[i], memo) + rods[i], \
+            memo[(i, difference)] = max(dp(i + 1, difference, memo),
+                                        dp(i + 1, difference + rods[i], memo) + rods[i],
                                         dp(i + 1, difference - rods[i], memo))
             return memo[(i, difference)]        
         return dp(0, 0, {})
