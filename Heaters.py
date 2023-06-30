2338f8489a22ab8b22630320cb7ef76b73899c8b
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-06-29 23:41:25 -0400

    Update Heaters.py

--- Heaters.py
+++ Heaters.py
@@ -1,5 +1,37 @@
 https://leetcode.com/problems/heaters/
+https://www.youtube.com/watch?v=K-Jz5eB1NIw&t=14s
 
+# https://www.youtube.com/watch?v=K-Jz5eB1NIw&t=14s
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        heaters.sort()    # n logn
+        res = 0
+        
+        # We want to find the shortest radius for each house
+        # Then we want to pick the max from these shortest radii.
+        # m logn
+        for house in houses:   
+            res = max(res, self.find_shortest_radius(heaters, house))  
+        
+        # (m + n) logn
+        return res
+
+    # bisect left
+    def find_shortest_radius(self, heaters, house):
+        l, r = -1, len(heaters)
+        shortest_radius = math.inf
+        
+        while l + 1 != r:
+            m = (l + r) // 2
+            shortest_radius = min(shortest_radius, abs(house - heaters[m]))
+            
+            if heaters[m] < house:
+                l = m
+            else:
+                r = m
+        
+        return shortest_radius
+------------------------------------------------------------------------------------
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         # Sort the houses and heaters
