a38f44bb3007ee1cfd1d8e6458bf4d4acba062a7
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-07-02 01:18:01 -0400

    Update Time Needed to Inform All Employees.py

--- Time Needed to Inform All Employees.py
+++ Time Needed to Inform All Employees.py
@@ -1,17 +1,17 @@
 https://leetcode.com/problems/time-needed-to-inform-all-employees/
 
 class Solution:
-    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+    def numOfMinutes(self, n: int, root_id: int, manager: List[int], time_needed_to_inform_workers: List[int]) -> int:
         graph = defaultdict(list)
         for employee_id in range(n):
-            if employee_id == headID:
+            if employee_id == root_id:
                 continue
             manager_id = manager[employee_id]
-            graph[manager_id].append((employee_id, informTime[manager_id]))
+            graph[manager_id].append((employee_id, time_needed_to_inform_workers[manager_id]))
         
         total_time = 0
         
-        queue = deque([(headID, 0)])
+        queue = deque([(root_id, 0)])
         while queue:
             employee_id, time = queue.popleft()
             total_time = max(total_time, time)    
