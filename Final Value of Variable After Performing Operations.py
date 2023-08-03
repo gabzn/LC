541173b747fb152cfd2c7b95414043e2f0967301
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-03 19:58:48 -0400

    Create Final Value of Variable After Performing Operations.py

---
+++ Final Value of Variable After Performing Operations.py
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        res = 0
+        for operation in operations:
+            if '-' in operation:
+                res -= 1
+            if '+' in operation:
+                res += 1
+        return res
