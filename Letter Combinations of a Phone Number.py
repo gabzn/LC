2373b9324b4ab5ee77b0a000bf24bfa7044f3354
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-03 00:50:14 -0400

    Update Letter Combinations of a Phone Number.py

--- Letter Combinations of a Phone Number.py
+++ Letter Combinations of a Phone Number.py
@@ -1,5 +1,35 @@
 https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-  
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        
+        LEN = len(digits)
+        digit_letter_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }    
+        combinations = []
+        
+        def backtrack(index, current):
+            if index == LEN:
+                combinations.append(current)
+                return
+            
+            digit = digits[index]
+            for char in digit_letter_map[digit]:
+                backtrack(index + 1, current + char)
+        
+        backtrack(0, '')
+        return combinations
+------------------------------------------------------------------------------------
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
