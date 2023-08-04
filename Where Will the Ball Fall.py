99295f0a6e0b497afcd87eb053a72369034911a8
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-04 01:02:27 -0400

    Update Where Will the Ball Fall.py

--- Where Will the Ball Fall.py
+++ Where Will the Ball Fall.py
@@ -1,5 +1,35 @@
 https://leetcode.com/problems/where-will-the-ball-fall/
-  
+
+class Solution:
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        M, N = len(grid), len(grid[0])
+        answer = [-1 for _ in range(N)]
+                
+        def dfs(x, y):
+            stack = [(x, y)]
+            res = -1
+            
+            while stack:
+                a, b = stack.pop()
+                if a == M:
+                    res = b
+                    break
+                
+                # if current cell is 1, check if the cell to the right is -1 or wall
+                if grid[a][b] == 1 and (b + 1 == N or grid[a][b+1] == -1):
+                    return -1
+                    
+                # if current cell is -1, check if the cell to the left is 1 or wall
+                if grid[a][b] == -1 and (b - 1 == -1 or grid[a][b-1] == 1):
+                    return -1
+                                
+                stack.append((a+1, b+1) if grid[a][b] == 1 else (a+1, b-1))
+            return res
+        
+        for index in range(N):
+            answer[index] = dfs(0, index)
+        return answer
+--------------------------------------------------------------------------------------
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
         R, C = len(grid), len(grid[0])
