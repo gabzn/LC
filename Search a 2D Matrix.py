aa966dc9ca496b60463b8a6b28b938e2e95a4d94
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-07 01:33:29 -0400

    Update Search a 2D Matrix.py

--- Search a 2D Matrix.py
+++ Search a 2D Matrix.py
@@ -1,16 +1,43 @@
 https://leetcode.com/problems/search-a-2d-matrix/
-  
-Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
-  Integers in each row are sorted from left to right. 
-  The first integer of each row is greater than the last integer of the previous row.
-  
-Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
-Output: true
 
-Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
-Output: false
-
-# Solution #1
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        # First, binary search the row the target is in
+        target_row = -1
+        left, right = 0, ROWS - 1
+        while left <= right:
+            mid = (left + right) // 2
+            
+            row_mid = matrix[mid]
+            if row_mid[0] <= target <= row_mid[-1]:
+                target_row = mid
+                break
+            
+            if row_mid[-1] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        if target_row == -1:
+            return False
+        
+        # Once we found the row that target is in, we binary search on that row
+        left, right = 0, COLS - 1
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if matrix[target_row][mid] == target:
+                return True
+            
+            if matrix[target_row][mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return False
+-----------------------------------------------------------------------------------------------
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
@@ -29,40 +56,4 @@ class Solution:
                         left = mid + 1
                     else:
                         right = mid - 1
-        return False
------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# Solution #2
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        top_row, bottom_row = 0, len(matrix) - 1
-        
-        # First perform BS on the rows to find the row of which target is in range.
-        while top_row <= bottom_row:
-            mid = (top_row + bottom_row) // 2    
-            mid_row = matrix[mid]            
-            if mid_row[0] <= target <= mid_row[-1]:
-                break
-                
-            if mid_row[0] < target:
-                top_row = mid + 1
-            else:
-                bottom_row = mid - 1
-                
-        if top_row > bottom_row:
-            return False
-        
-        # Then perform another BS on the target row to find the target.
-        # O(2logn)
-        target_row = matrix[(top_row + bottom_row) // 2]
-        left, right = 0, len(target_row) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if target_row[mid] == target:
-                return True
-              
-            if target_row[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
         return False
