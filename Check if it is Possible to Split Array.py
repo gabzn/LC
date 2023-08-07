a29a113d19d2707a78b68d985d99d97d748911ad
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-07 01:20:54 -0400

    Update Check if it is Possible to Split Array.py

--- Check if it is Possible to Split Array.py
+++ Check if it is Possible to Split Array.py
@@ -5,6 +5,33 @@ class Solution:
     def canSplitArray(self, nums: List[int], m: int) -> bool:
         if len(nums) <= 2:
             return True
+
+        N = len(nums)
+        
+        @cache
+        def dp(start, end):
+            # If there's only 1 digit, True
+            if end - start == 1:
+                return True
+            # If there are 2 digits and the sum is greater than m, True
+            if end - start == 2:
+                return sum(nums[start: end]) >= m
+
+            for i in range(start + 1, end):
+                if dp(start, i) and dp(i, end):
+                    return True
+            return False
+               
+        for i in range(1, N):
+            if dp(0, i) and dp(i, N):
+                return True
+            
+        return False
+---------------------------------------------------------------------
+class Solution:
+    def canSplitArray(self, nums: List[int], m: int) -> bool:
+        if len(nums) <= 2:
+            return True
           
         for i in range(len(nums)-1):
             if nums[i] + nums[i+1] >= m:
