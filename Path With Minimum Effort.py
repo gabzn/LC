5e4ab895b3f4fccbc58cef9ccf256c18b3b41502
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-09 22:08:30 -0400

    Update Path With Minimum Effort.py

--- Path With Minimum Effort.py
+++ Path With Minimum Effort.py
@@ -2,6 +2,49 @@ https://leetcode.com/problems/path-with-minimum-effort/
 
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        def is_valid_cell(x, y):
+            return 0 <= x < ROWS and 0 <= y < COLS
+        
+        def can_reach_end_with_effort(m):
+            queue = deque([(0, 0, heights[0][0])])
+            visited_cells = set()
+            
+            while queue:
+                x, y, height = queue.popleft()
+                if x == ROWS - 1 and y == COLS - 1:
+                    return True
+                
+                if (x, y) in visited_cells:
+                    continue
+                visited_cells.add((x, y))
+                
+                for offset_x, offset_y in DIRECTIONS:
+                    neighbour_x, neighbour_y = x + offset_x, y + offset_y
+                    
+                    if is_valid_cell(neighbour_x, neighbour_y):
+                        neighbour_height = heights[neighbour_x][neighbour_y]
+                        if abs(neighbour_height - height) <= m:
+                            queue.append((neighbour_x, neighbour_y, neighbour_height))
+            
+            return False
+        
+        ROWS, COLS = len(heights), len(heights[0])
+        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        l, r = -1, 10**6 + 1
+        
+        while l + 1 != r:
+            m = (l + r) // 2
+            
+            if can_reach_end_with_effort(m):
+                r = m
+            else:
+                l = m
+        
+        return r
+----------------------------------------------------------------------------
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         l, r = -1, 1000002
         
