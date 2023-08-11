2e8777c10408f2af5999551ac53c73327fff123c
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-11 15:28:35 -0400

    Update Coin Change II.py

--- Coin Change II.py
+++ Coin Change II.py
@@ -1,5 +1,26 @@
 https://leetcode.com/problems/coin-change-ii/
 
+# https://www.youtube.com/watch?v=EqBJ7ogzrYI&t=651s
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        LEN = len(coins)
+        
+        @cache
+        def dp(index, amt):
+            if amt < 0 or index == LEN:
+                return 0            
+            if amt == 0:
+                return 1
+            
+            # 2 OPTIONS
+            # 1: We take the current coin, and we have the option to take it again
+            # 2: We skip the current coin, and we move onto the next coin
+            take_current_coin = dp(index, amt - coins[index])
+            skip_current_coin = dp(index + 1, amt)
+            return take_current_coin + skip_current_coin
+            
+        return dp(0, amount)
+----------------------------------------------------------------------------------------------
 # https://www.youtube.com/watch?v=DJ4a7cmjZY0
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
