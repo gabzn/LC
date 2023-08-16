84cd9800dfa3f0089e0f0c370015ad99f4bc1236
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-15 21:22:19 -0400

    Update Sliding Window Maximum.py

--- Sliding Window Maximum.py
+++ Sliding Window Maximum.py
@@ -1,5 +1,31 @@
 https://leetcode.com/problems/sliding-window-maximum/
-  
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        queue = deque()
+        
+        for index, num in enumerate(nums):
+            # We want at least k elements in the queue
+            if index < k - 1:
+                # Keep the queue decreasing
+                while queue and num > queue[-1][0]:
+                    queue.pop()
+                queue.append((num, index))
+            else:
+                # Check if the leftmost digit is out of bound
+                if queue and index - queue[0][1] == k:
+                    queue.popleft()
+                
+                # Keep the queue decreasing
+                while queue and num > queue[-1][0]:
+                    queue.pop()
+                
+                queue.append((num, index))
+                res.append(queue[0][0])
+                
+        return res
+---------------------------------------------------------------------------
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         queue = collections.deque()
