b106f2b9142968ca7f347673dce6c092ce70e042
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-27 23:39:21 -0400

    Update Binary Tree Right Side View.py

--- Binary Tree Right Side View.py
+++ Binary Tree Right Side View.py
@@ -1,36 +1,26 @@
 https://leetcode.com/problems/binary-tree-right-side-view/
-  
-Given the root of a binary tree, imagine yourself standing on the right side of it, 
-return the values of the nodes you can see ordered from top to bottom.
-
-Input: root = [1,2,3,null,5,null,4]
-Output: [1,3,4]
-  
-from collections import deque
 
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
-            return []
+            return root
         
-        # BFS Problem
         res = []
-        queue = deque()
-        queue.append(root)
+        queue = deque([root])
         
         while queue:
-            # num_of_nodes_to_pop tells us how many pop operations we need to do which is equal to the number of nodes in the current level.
-            # We need to pop every single node on the current level and store its non-null children.
-            num_of_nodes_to_pop = len(queue)
+            LEN = len(queue)
+            current_level = []
             
-            # Right side view means the rightmost node in the current level which is the last node in the queue.
-            rightmost_node = queue[-1]
-            res.append(rightmost_node.val)
-            
-            for _ in range(num_of_nodes_to_pop):    
+            for _ in range(LEN):
                 node = queue.popleft()
+                current_level.append(node.val)
+                
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
+            
+            res.append(current_level[-1])
+            
         return res
