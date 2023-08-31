5dd788628170cdc3063e52f7b2b62cc595764a41
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-08-31 00:07:48 -0400

    Update Minimum Number of Taps to Open to Water a Garden.py

--- Minimum Number of Taps to Open to Water a Garden.py
+++ Minimum Number of Taps to Open to Water a Garden.py
@@ -2,6 +2,23 @@ https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/
 
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
+        # dp[i] is the min number of taps needed to water 1 to i
+        dp = [math.inf] * (n + 1)
+        dp[0] = 0
+        
+        for i in range(n + 1):
+            dist = ranges[i]
+            
+            left = max(0, i - dist)
+            right = min(n, i + dist)
+            
+            for j in range(left, right + 1):
+                dp[right] = min(dp[j] + 1, dp[right])
+        
+        return dp[-1] if dp[-1] != math.inf else -1
+-------------------------------------------------------------------------------------
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
         """
         Visualizing the intervals helps!
         Be greedy and always pick the interval that covers more area
