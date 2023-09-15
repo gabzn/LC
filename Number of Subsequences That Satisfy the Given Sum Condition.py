50fbc28611807f6282728e6c982b74d6d290b1ca
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-09-15 00:58:50 -0400

    Update Number of Subsequences That Satisfy the Given Sum Condition.py

--- Number of Subsequences That Satisfy the Given Sum Condition.py
+++ Number of Subsequences That Satisfy the Given Sum Condition.py
@@ -17,3 +17,31 @@ class Solution:
             res += pow(2, r - l) % MOD
                   
         return res % MOD
+-----------------------------------------------------------------------------
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        def find_right_bound(smallest, left, right):
+            
+            while left + 1 != right:
+                mid = (left + right) // 2
+                if nums[mid] + smallest > target:
+                    right = mid
+                else:
+                    left = mid
+        
+            return left
+            
+        MOD = (10**9) + 7
+        LEN = len(nums)
+        
+        nums.sort()
+        res, r = 0, LEN - 1
+        
+        for l in range(len(nums)):
+            r = find_right_bound(nums[l], l-1, LEN)
+            if r < l:
+                break
+                
+            res += pow(2, r - l) % MOD
+                  
+        return res % MOD
