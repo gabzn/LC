0cc936f8119eba8e683957ab0e591bec87f80504
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-09-23 21:56:46 -0400

    Update Count Complete Subarrays in an Array.py

--- Count Complete Subarrays in an Array.py
+++ Count Complete Subarrays in an Array.py
@@ -3,6 +3,28 @@ https://leetcode.com/problems/count-complete-subarrays-in-an-array/
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
         LEN = len(nums)
+        TOTAL_UNIQUE = len(set(nums))
+        
+        window_freq = defaultdict(int)
+        res, left = 0, 0
+        
+        for right in range(LEN):
+            window_freq[nums[right]] += 1
+            
+            while len(window_freq) == TOTAL_UNIQUE:
+                res += (LEN - right)
+                
+                window_freq[nums[left]] -= 1
+                if window_freq[nums[left]] == 0:
+                    del window_freq[nums[left]]
+                
+                left += 1
+        
+        return res
+---------------------------------------------------------------------------------------
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        LEN = len(nums)
         
         unique_nums = len(set(nums))
         res = 0
