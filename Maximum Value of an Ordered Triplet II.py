28e678426d73b2ecfe75a48a616dcb165518b1a1
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-10-03 21:45:06 -0400

    Update Maximum Value of an Ordered Triplet II.py

--- Maximum Value of an Ordered Triplet II.py
+++ Maximum Value of an Ordered Triplet II.py
@@ -2,6 +2,25 @@ https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/
 
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
+        def compute_prefix_max(lst):
+            pref = [0]
+            for i in range(1, len(lst)):
+                pref.append(max(pref[-1], lst[i-1]))
+            return pref
+        
+        prefix_max = compute_prefix_max(nums)
+        suffix_max = compute_prefix_max(nums[::-1])[::-1]
+        
+        res = 0
+        
+        # Fix i and k, treat each num in nums as new j
+        for j in range(1, len(nums) - 1):
+            res = max(res, (prefix_max[j] - nums[j]) * suffix_max[j])
+        
+        return res
+--------------------------------------------------------------------------------------------------
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
         res, max_i, max_i_minus_j = 0, 0, 0
         
         """
