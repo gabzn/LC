7168c0c3deb97af64930f9438c534ca94fb25b84
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-11-15 00:33:07 -0500

    Update Maximum Element After Decreasing and Rearranging.py

--- Maximum Element After Decreasing and Rearranging.py
+++ Maximum Element After Decreasing and Rearranging.py
@@ -2,6 +2,16 @@ https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
 
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        
+        arr[0] = 1
+        for idx in range(1, len(arr)):
+            arr[idx] = min(arr[idx - 1] + 1, arr[idx])
+        
+        return arr[-1]
+----------------------------------------------------------------------------------------------
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
         LEN = len(arr)
         
         arr.sort()
