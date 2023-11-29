8a6005459520844afc04e2a81815a6f0ef5bc0ae
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-11-29 00:29:11 -0500

    Create Find Words Containing Character.py

---
+++ Find Words Containing Character.py
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/find-words-containing-character/
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        res = []
+        for idx, word in enumerate(words):
+            if x in word:
+                res.append(idx)
+                
+        return res
