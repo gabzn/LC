0f6851c762700902b26faa5ff13174f747844be3
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-12-12 16:34:24 -0500

    Update Split Array Largest Sum.py

--- Split Array Largest Sum.py
+++ Split Array Largest Sum.py
@@ -11,7 +11,13 @@ class Solution:
                 else:
                     splits += 1
                     running_sum = num
- 
+            
+            """
+            If the total number of splits is less than k, we need to decrement mid to make each split smaller
+            If the total number of splits is equal to k, we still decrement mid to see if it can be smaller
+            
+            If the total number of splits is greater than k, that means the sum is too small. We need to look for bigger mid.
+            """
             return splits + 1 <= k
 
         left, right = max(nums) - 1, sum(nums) + 1
