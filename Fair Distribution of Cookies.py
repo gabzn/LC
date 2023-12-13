0ac26106c4b1b3cfc9b358fb1f10cfa2dce85ef2
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-12-12 22:34:00 -0500

    Update Fair Distribution of Cookies.py

--- Fair Distribution of Cookies.py
+++ Fair Distribution of Cookies.py
@@ -3,6 +3,39 @@ https://www.youtube.com/watch?v=t-BOmhv0WTw
 
 class Solution:
     def distributeCookies(self, cookies: List[int], children: int) -> int:
+        def can_distribute_at_most_mid_cookies_to_children(assignments, index, mid):
+            if index == LEN:
+                return True
+            
+            for child in range(children):
+                if assignments[child] + cookies[index] > mid:
+                    continue
+                
+                assignments[child] += cookies[index]
+                
+                if can_distribute_at_most_mid_cookies_to_children(assignments, index + 1, mid):
+                    return True
+                
+                assignments[child] -= cookies[index]
+            
+            return False
+        
+        LEN = len(cookies)
+        left, right = max(cookies) - 1, sum(cookies) + 1
+        
+        while left + 1 != right:
+            mid = (left + right) // 2
+            assignments = [0 for _ in range(children)]
+            
+            if can_distribute_at_most_mid_cookies_to_children(assignments, 0, mid):
+                right = mid
+            else:
+                left = mid
+        
+        return right
+-------------------------------------------------------------------------------
+class Solution:
+    def distributeCookies(self, cookies: List[int], children: int) -> int:
         LEN = len(cookies)
         INF = math.inf
         assignments = [0 for _ in range(children)]
