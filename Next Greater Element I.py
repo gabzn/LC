98dacac2c7dd7f4b10fed50095bf22b135f049a4
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-12-17 17:33:07 -0500

    Update Next Greater Element I.py

--- Next Greater Element I.py
+++ Next Greater Element I.py
@@ -1,7 +1,25 @@
 https://leetcode.com/problems/next-greater-element-i/
-  
-from collections import defaultdict
 
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num_to_next_greater = {}
+        stack = []
+        for num in nums2:
+            while stack and num > stack[-1]:
+                prev_num = stack.pop()
+                num_to_next_greater[prev_num] = num
+            
+            stack.append(num)
+        
+        while stack:
+            num_to_next_greater[stack.pop()] = -1
+        
+        res = []
+        for num in nums1:
+            res.append(num_to_next_greater[num])
+        
+        return res
+-------------------------------------------------------------------------------------------------
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res, stack = [-1] * len(nums1), []        
@@ -24,23 +42,3 @@ class Solution:
             num_ind_dict[num] = index  
         
         return num_ind_dict
-    
-#         res = []
-#         num_ind_dict = defaultdict(int)
-        
-#         for index, num in enumerate(nums2):
-#             num_ind_dict[num] = index
-        
-#         for num in nums1:
-#             ind_in_num2 = num_ind_dict[num]
-#             previous_len = len(res)
-            
-#             for i in range(ind_in_num2+1, len(nums2)):
-#                 if nums2[i] > num:
-#                     res.append(nums2[i])
-#                     break
-            
-#             if previous_len == len(res):
-#                 res.append(-1)
-                
-#         return res
