73cdbcbd3d79df8e2fffb9973df49e625010bca0
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-12-20 00:08:35 -0500

    Update Minimum Cost to Make Array Equalindromic.py

--- Minimum Cost to Make Array Equalindromic.py
+++ Minimum Cost to Make Array Equalindromic.py
@@ -2,6 +2,32 @@ https://leetcode.com/problems/minimum-cost-to-make-array-equalindromic/
 
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
+        def is_palindromic(x):
+            return x == x[::-1]
+        
+        LEN = len(nums)
+        
+        nums.sort()
+        
+        mid = nums[LEN // 2]
+        low = int(math.floor(mid))
+        high = int(math.ceil(mid))
+        
+        while not is_palindromic(str(low)):
+            low -= 1
+        
+        while not is_palindromic(str(high)):
+            high += 1
+        
+        low_cost = high_cost = 0
+        for num in nums:
+            low_cost += abs(low - num)
+            high_cost += abs(high - num)
+
+        return min(low_cost, high_cost)
+--------------------------------------------------------------------------------------------
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
         """
         1: choose a num in nums and X
         2: add |num - X| to total cost
