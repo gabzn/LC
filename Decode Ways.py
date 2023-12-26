a0def3c825b8bc95f00968035acc4c40ee288d75
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-12-26 00:08:38 -0500

    Update Decode Ways.py

--- Decode Ways.py
+++ Decode Ways.py
@@ -4,6 +4,25 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         LEN = len(s)
         
+        @cache
+        def dp(i):
+            if i == LEN:
+                return 1
+            if s[i] == '0':
+                return 0
+            
+            res = dp(i + 1)
+            if i + 1 < LEN and int(s[i] + s[i + 1]) <= 26:
+                res += dp(i + 2)
+            
+            return res
+        
+        return dp(0)
+-------------------------------------------------------------------------------
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        LEN = len(s)
+        
         # dp(i) returns the number of ways to decode starting at index i
         def dp(i, memo):
             # If the index is out of bound, that means this str was decoded
