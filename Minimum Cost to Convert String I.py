43545da57821f6d3f1318ffd9876adac60ea5818
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-12-28 19:06:55 -0500

    Update Minimum Cost to Convert String I.py

--- Minimum Cost to Convert String I.py
+++ Minimum Cost to Convert String I.py
@@ -5,6 +5,41 @@ class Solution:
         def index(char):
             return ord(char) - ord('a')
         
+        # Compute the shortest paths for every single pair of nodes
+        # Since there's only 26 English letters, using Floyd-Warsall only takes 26^3 which is 17576
+        distances = [[math.inf if i != j else 0 for j in range(26)] for i in range(26)]
+                
+        # Initialize initial values from source to destination
+        for start, end, weight in zip(original, changed, cost):
+            # i and j means from i to j
+            i, j = map(index, [start, end])
+            distances[i][j] = min(distances[i][j], weight)
+        
+        # Run Floyd-Warsall
+        for through in range(26):
+            for start in range(26):
+                for end in range(26):
+                    distances[start][end] = min(distances[start][end], \
+                                                distances[start][through] + distances[through][end])
+        
+        res = 0
+        
+        for s, t in zip(source, target):
+            start, end = map(index, [s, t])
+            
+            distance = distances[start][end]
+            if distance == math.inf:
+                return -1
+            
+            res += distance
+            
+        return res
+------------------------------------------------------------------------------------------------
+class Solution:
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        def index(char):
+            return ord(char) - ord('a')
+        
         # Dijkstra compute the min distance from "s" to all others 
         def dijkstra(s):
             heap = [(0, s)]
