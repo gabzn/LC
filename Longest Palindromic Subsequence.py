a1c7cbdf2880239763c4e85ffed2fbe4d0bfe8f5
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-12-28 22:40:01 -0500

    Update Longest Palindromic Subsequence.py

--- Longest Palindromic Subsequence.py
+++ Longest Palindromic Subsequence.py
@@ -2,6 +2,22 @@ https://leetcode.com/problems/longest-palindromic-subsequence/
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
+        @cache
+        def dp(left, right):
+            if left > right:
+                return 0
+            if left == right:
+                return 1
+            
+            if s[left] == s[right]:
+                return 2 + dp(left + 1, right - 1)
+            else:
+                return max(dp(left + 1, right), dp(left, right - 1))
+        
+        return dp(0, len(s) - 1)
+----------------------------------------------------------------------------------
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
     
         def dp(left, right, memo):
             if left > right:
