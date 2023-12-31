638c14f8e1aee09b3529f770f96dfe4f890f1c22
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-12-31 01:15:24 -0500

    Create Find Longest Special Substring That Occurs Thrice I.py

---
+++ Find Longest Special Substring That Occurs Thrice I.py
@@ -0,0 +1,34 @@
+https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-i/
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        LEN = len(s)
+        
+        c = {}
+        left = 0
+        
+        while left < LEN:
+            right = left + 1
+            
+            while right < LEN and s[left] == s[right]:
+                right += 1
+            
+            if s[left] not in c:
+                c[s[left]] = [0] * (LEN + 1)
+
+            sub_len = right - left
+            for i in range(1, sub_len + 1):
+                c[s[left]][i] += sub_len - i + 1
+            
+            left = right
+                
+        res = -1
+        
+        for _, lst in c.items():
+            for idx in range(1, len(lst)):
+                if lst[idx] == 0:
+                    break
+                if lst[idx] >= 3:
+                    res = max(res, idx)
+        
+        return res
