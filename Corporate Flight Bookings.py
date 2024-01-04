2dc110bea5b70570047f8079469880571362585a
Gabriel <58453421+gabzn@users.noreply.github.com>
2024-01-03 22:00:18 -0500

    Update Corporate Flight Bookings.py

--- Corporate Flight Bookings.py
+++ Corporate Flight Bookings.py
@@ -2,6 +2,22 @@ https://leetcode.com/problems/corporate-flight-bookings/
 
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:        
+        diff_lst = [0] * (n + 2)
+        for f, l, s in bookings:
+            diff_lst[f] += s
+            diff_lst[l + 1] -= s
+            
+        res = []
+        seats = 0      
+        
+        for i in range(1, n + 1):
+            seats += diff_lst[i]
+            res.append(seats)
+                
+        return res
+---------------------------------------------------------------------------------------
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:        
         diff_lst = []
         for f, l, s in bookings:
             # (f - 1) is for indexing purposes
