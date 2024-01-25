fe2b7c960f5bf93daadd7a4136cf6f2596dd4841
Gabriel <58453421+gabzn@users.noreply.github.com>
2024-01-25 00:12:08 -0500

    Update Number of People Aware of a Secret.py

--- Number of People Aware of a Secret.py
+++ Number of People Aware of a Secret.py
@@ -1,4 +1,5 @@
 https://leetcode.com/problems/number-of-people-aware-of-a-secret/
+https://www.youtube.com/watch?v=dalyTgkiH0s
 
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
@@ -8,6 +9,7 @@ class Solution:
         dp[1] = 1
         
         for i in range(1, n + 1):
+            # The i-th person can start sharing at (i + delay) day up to but not including (i + forget)
             for person in range(i + delay, min(i + forget, n + 1)):
                 dp[person] += dp[i]
         
