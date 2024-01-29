c874623ed83a338fc2ce0788e720306582a3589f
Gabriel <58453421+gabzn@users.noreply.github.com>
2024-01-28 20:35:42 -0500

    Update Paint House.py

--- Paint House.py
+++ Paint House.py
@@ -1,5 +1,26 @@
 https://leetcode.com/problems/paint-house/
-  
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        LEN = len(costs)
+        
+        dp = [[0, 0, 0] for _ in range(LEN)]
+        dp[0][0] = costs[0][0]
+        dp[0][1] = costs[0][1]
+        dp[0][2] = costs[0][2]
+        
+        for i in range(1, LEN):
+            # paint the current house 0, depending on the previous 1 and 2
+            dp[i][0] = costs[i][0] + min(dp[i-1][1], dp[i-1][2])
+            
+            # paint the current house 1, depending on the previous 0 and 2
+            dp[i][1] = costs[i][1] + min(dp[i-1][0], dp[i-1][2])
+            
+            # paint the current house 2, depending on the previous 0 and 1            
+            dp[i][2] = costs[i][2] + min(dp[i-1][0], dp[i-1][1])
+        
+        return min(dp[-1])
+-----------------------------------------------------------------------------------------------
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
         N = len(costs)
