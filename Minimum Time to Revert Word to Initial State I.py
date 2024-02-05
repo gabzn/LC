a3b1d49466903d7930172e50ab7ac886d067c590
Gabriel <58453421+gabzn@users.noreply.github.com>
2024-02-04 20:31:04 -0500

    Create Minimum Time to Revert Word to Initial State I.py

---
+++ Minimum Time to Revert Word to Initial State I.py
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/minimum-time-to-revert-word-to-initial-state-i/
+
+class Solution:
+    def minimumTimeToInitialState(self, word: str, k: int) -> int:
+        LEN = len(word)
+        res = 0
+        
+        for i in range(k, LEN, k):
+            res += 1
+            
+            if word[i:] == word[:LEN - i]:
+                return res
+        
+        return res + 1
