ce34666a4174adf364bfb47c2f9709da7aa61637
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-02-25 22:29:51 -0500

    Update Find the Largest Area of Square Inside Two Rectangles.py

--- Find the Largest Area of Square Inside Two Rectangles.py
+++ Find the Largest Area of Square Inside Two Rectangles.py
@@ -10,7 +10,7 @@ class Solution:
             ax1, ay1 = bottom[i]
             ax2, ay2 = top[i]
             
-            for j in range(i+1, LEN):
+            for j in range(i + 1, LEN):
                 bx1, by1 = bottom[j]
                 bx2, by2 = top[j]
                 
@@ -27,4 +27,26 @@ class Solution:
                 
                     res = max(res, min(x, y) ** 2)
             
+        return res
+---------------------------------------------------------------------------------------------------
+class Solution:
+    def largestSquareArea(self, bottom: List[List[int]], top: List[List[int]]) -> int:
+        LEN = len(top)
+        
+        res = 0
+        
+        for i in range(LEN):
+            ix_start, ix_end = bottom[i][0], top[i][0]
+            iy_start, iy_end = bottom[i][1], top[i][1]
+            
+            for j in range(i + 1, LEN):
+                jx_start, jx_end = bottom[j][0], top[j][0]
+                jy_start, jy_end = bottom[j][1], top[j][1]
+                
+                x = min(ix_end, jx_end) - max(ix_start, jx_start)
+                y = min(iy_end, jy_end) - max(iy_start, jy_start)
+                
+                # If there are no overlapping, min(x, y) will either give negative or 0
+                res = max(res, max(min(x, y), 0) ** 2)
+                    
         return res
