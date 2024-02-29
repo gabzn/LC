9fe17d0434e0b60e26e0fffdceb96a4c80b42858
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-02-28 23:24:31 -0500

    Update Find the Number of Ways to Place People I.py

--- Find the Number of Ways to Place People I.py
+++ Find the Number of Ways to Place People I.py
@@ -3,6 +3,43 @@ https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
         LEN = len(points)
+        
+        to_map = defaultdict(set)
+        from_map = defaultdict(set)
+        
+        for i in range(LEN):
+            xa, ya = points[i]
+            
+            for j in range(LEN):
+                if i == j:
+                    continue
+                    
+                xb, yb = points[j]
+                
+                if xa <= xb and ya >= yb:
+                    to_map[i].add(j)
+                    from_map[j].add(i)
+                
+        res = 0
+        
+        for i in range(LEN):
+            xa, ya = points[i]
+            
+            for j in range(LEN):
+                if i == j:
+                    continue
+                xb, yb = points[j]
+                
+                if xa <= xb and ya >= yb:
+                    common = from_map[j].intersection(to_map[i])
+                    if not common:
+                        res += 1
+                    
+        return res
+----------------------------------------------------------------------------
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        LEN = len(points)
         res = 0        
 
         for i in range(LEN):
