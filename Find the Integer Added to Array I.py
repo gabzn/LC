32de4d9508772b70ce2f7c12e54f8081f71a7de4
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-04-28 00:25:35 -0400

    Create Find the Integer Added to Array I.py

---
+++ Find the Integer Added to Array I.py
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/find-the-integer-added-to-array-i/
+
+class Solution:
+    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:        
+        return sorted(nums2)[0] - sorted(nums1)[0]
