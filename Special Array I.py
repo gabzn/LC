b6d993cd41091847b0025bc7a5c775623a2daaa7
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-05-19 23:34:42 -0400

    Update Special Array I.py

--- Special Array I.py
+++ Special Array I.py
@@ -2,12 +2,17 @@ https://leetcode.com/problems/special-array-i/
 
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        N = len(nums)
-        prev = nums[0] % 2
-        
-        for i in range(1, N):
-            if nums[i] % 2 == prev:
+        for x, y in pairwise(nums):
+            if x % 2 == y % 2:
                 return False
-            prev = nums[i] % 2
-            
+        return True
+--------------------------------------------------------
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        N = len(nums)
+    
+        for i in range(1, N):
+            if nums[i] % 2 == nums[i-1] % 2:
+                return False
+                
         return True
