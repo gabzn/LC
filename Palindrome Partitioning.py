5f277076416573d4770f1490d380839eaae19fb1
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-05-22 23:31:41 -0400

    Update Palindrome Partitioning.py

--- Palindrome Partitioning.py
+++ Palindrome Partitioning.py
@@ -1,5 +1,24 @@
 https://leetcode.com/problems/palindrome-partitioning/description/
-  
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def backtrack(i, current):
+            if i == N:
+                res.append(current[::])
+                return
+            
+            for idx in range(i, N):
+                sub_s = s[i: idx + 1]
+                if sub_s == sub_s[::-1]:
+                    current.append(sub_s)
+                    backtrack(idx + 1, current)
+                    current.pop()
+            
+        N = len(s)
+        res = []
+        backtrack(0, [])
+        return res
+--------------------------------------------------------------------------------
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         return self.backtrack(s, 0, [], [])
