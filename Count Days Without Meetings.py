b1d0773b9fa3639833daa62beb784777564a2077
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-06-02 19:50:26 -0400

    Update Count Days Without Meetings.py

--- Count Days Without Meetings.py
+++ Count Days Without Meetings.py
@@ -1,5 +1,31 @@
 https://leetcode.com/problems/count-days-without-meetings/
 
+
+class Solution:
+    # https://leetcode.com/problems/merge-intervals/
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        """
+        Sort the intervals by their start time.
+        Loop through the intervals starting from the second interval. (Kind of 2-pointer)
+        Compare the current start to the previous end to see if they are overlapping.
+        """
+        intervals.sort()
+        res = []
+
+        for start, end in intervals:
+            if res and res[-1][1] >= start:
+                res[-1][1] = max(res[-1][1], end)
+            else:
+                res.append([start, end])
+        
+        return res
+    
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        for start, end in self.merge(meetings):
+            days -= (end - start + 1)
+
+        return days
+-----------------------------------------------------------------------------
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
         N = len(meetings)
