542d38f48a1ee1dc7d82dc79d732b5e115d936dd
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-06-02 22:07:45 -0400

    Update Merge Triplets to Form Target Triplet.py

--- Merge Triplets to Form Target Triplet.py
+++ Merge Triplets to Form Target Triplet.py
@@ -2,10 +2,9 @@ https://leetcode.com/problems/merge-triplets-to-form-target-triplet/
 
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        N = len(triplets)
-        x, y, z = target
-        
+        x, y, z = target        
         valid_triplets = []
+        
         for i, [a, b, c] in enumerate(triplets):
             if a > x or b > y or c > z:
                 continue
@@ -13,6 +12,7 @@ class Solution:
                 valid_triplets.append(i)
         
         res = [0] * 3
+        
         for i in range(3):
             for index in valid_triplets:
                 res[i] = max(res[i], triplets[index][i])
