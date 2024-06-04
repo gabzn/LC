c19b64ebfdc75ed43b9e02e8c387db9c24550c6a
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-06-03 23:22:49 -0400

    Update Sliding Subarray Beauty.py

--- Sliding Subarray Beauty.py
+++ Sliding Subarray Beauty.py
@@ -1,5 +1,38 @@
 https://leetcode.com/problems/sliding-subarray-beauty/
 
+from sortedcontainers import SortedList
+
+class Solution:
+    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+        N = len(nums)
+        sl = SortedList()
+        res = []
+        left = right = negative_count = 0
+
+        while right < N:
+            # If the window size > k, move left 1 unit to the right
+            if right - left + 1 > k:
+                sl.remove(nums[left])
+                if nums[left] < 0:
+                    negative_count -= 1
+                left += 1
+
+            # Add the current num to the window
+            if nums[right] < 0:
+                negative_count += 1
+            sl.add(nums[right])
+            
+            # If we have at least window size, add the xth smallest
+            if right >= k - 1:
+                if negative_count < x:
+                    res.append(0)
+                else:
+                    res.append(sl[x - 1])
+
+            right += 1
+
+        return res
+------------------------------------------------------------------------------
 class Solution:
     def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
         N = len(nums)
