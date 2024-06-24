050a7382cd7c5e2eb5a52ef2311fdbec79189a08
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-06-24 00:13:01 -0400

    Create Maximize Total Cost of Alternating Subarrays.py

---
+++ Maximize Total Cost of Alternating Subarrays.py
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/maximize-total-cost-of-alternating-subarrays/
+
+class Solution:
+    def maximumTotalCost(self, nums: List[int]) -> int:    
+        N = len(nums)
+        
+        """
+        dp[i][0] = the max cost from nums[0] to nums[i] and putting a + sign before nums[i]
+        dp[i][1] = the max cost from nums[0] to nums[i] and putting a - sign before nums[i]
+        
+        When can you pick + and -?
+            You can pick + for current when the previous is:
+                + -> means the current starts fresh
+                - -> means appending the current to previous
+            
+            You can pick - for current only when the previous is +
+        
+        dp[0][0] and dp[0][1] is always nums[0]
+        """
+        dp = [[0, 0] for _ in range(N)]
+        dp[0][0] = dp[0][1] = nums[0]
+        
+        for i in range(1, N):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]) + nums[i]
+            dp[i][1] = dp[i-1][0] - nums[i]
+        
+        return max(dp[-1])
