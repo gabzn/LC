5a9f2594e8f58f760d5c0ea2005f3acfd39de98d
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-07-21 23:31:35 -0400

    Update Find Valid Matrix Given Row and Column Sums.py

--- Find Valid Matrix Given Row and Column Sums.py
+++ Find Valid Matrix Given Row and Column Sums.py
@@ -2,6 +2,21 @@ https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
 
 class Solution:
     def restoreMatrix(self, row_sum: List[int], col_sum: List[int]) -> List[List[int]]:
+        ROWS = len(row_sum)
+        COLS = len(col_sum)
+        
+        res = [[0] * COLS for _ in range(ROWS)]
+        for i in range(ROWS):
+            for j in range(COLS):
+                val = min(row_sum[i], col_sum[j])
+                res[i][j] = val
+                row_sum[i] -= val
+                col_sum[j] -= val
+        
+        return res
+---------------------------------------------------------------------------------------
+class Solution:
+    def restoreMatrix(self, row_sum: List[int], col_sum: List[int]) -> List[List[int]]:
         res = [[0 for _ in range(len(col_sum))] for _ in range(len(row_sum))]
         row_heap = [(num, i) for i, num in enumerate(row_sum)]
         col_heap = [(num, i) for i, num in enumerate(col_sum)]
