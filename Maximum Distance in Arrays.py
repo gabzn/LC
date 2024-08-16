8942401c9369d8a8f1a1d14c9794d315acfd109a
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-08-15 21:36:00 -0400

    Update Maximum Distance in Arrays.py

--- Maximum Distance in Arrays.py
+++ Maximum Distance in Arrays.py
@@ -15,3 +15,24 @@ class Solution:
             prev_max = max(prev_max, cur_max)
             
         return res
+-----------------------------------------------------------------------------------
+class Solution:
+    def maxDistance(self, arrays: List[List[int]]) -> int:        
+        heap = []
+        for i, array in enumerate(arrays):
+            heappush(heap, (array[0], i))
+        
+        res = 0
+        for i, array in enumerate(arrays):
+            max_in_array = array[-1]
+            placeholder = []
+            
+            while heap and heap[0][1] == i:
+                placeholder.append(heappop(heap))
+            
+            res = max(res, abs(max_in_array - heap[0][0]))
+            
+            while placeholder:
+                heappush(heap, placeholder.pop())
+        
+        return res
