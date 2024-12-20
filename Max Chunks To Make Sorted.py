ea58258287be41faa895b2646174f20647a98da3
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-12-20 00:17:03 -0500

    Update Max Chunks To Make Sorted.py

--- Max Chunks To Make Sorted.py
+++ Max Chunks To Make Sorted.py
@@ -1,5 +1,21 @@
 https://leetcode.com/problems/max-chunks-to-make-sorted/
-  
+
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        stack = []
+        
+        for num in arr:
+            if len(stack) == 0 or stack[-1] < num:
+                stack.append(num)
+                continue
+
+            max_element_in_current_chunk = -1
+            while stack and stack[-1] > num:
+                max_element_in_current_chunk = max(max_element_in_current_chunk, stack.pop())
+            stack.append(max_element_in_current_chunk)
+        
+        return len(stack)
+-------------------------------------------------------
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         sorted_arr = sorted(arr)
