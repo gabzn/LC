c7b5b0101e9e33b2b7cc61d77368fc6b8722eb9f
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2024-12-21 00:29:09 -0500

    Update Bus Routes.py

--- Bus Routes.py
+++ Bus Routes.py
@@ -1,23 +1,23 @@
 https://leetcode.com/problems/bus-routes/
 
 class Solution:
-    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:    
-        stop_to_buses = defaultdict(list)
-        for idx, route in enumerate(routes):
-            for stop in route:
-                stop_to_buses[stop].append(idx)
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        stop_to_buses = defaultdict(set)
+        for i, stops in enumerate(routes):
+            for stop in stops:
+                stop_to_buses[stop].add(i)
         
         visited_buses = set()
-        visited_stops = set()
-        visited_stops.add(source)
-        
+        visited_stops = set([source])
         queue = deque([(source, 0)])
         while queue:
-            stop, buses_taken = queue.popleft()
+            stop, total_buses = queue.popleft()
             if stop == target:
-                return buses_taken
+                return total_buses
             
             for bus in stop_to_buses[stop]:
+                # We don't add the first bus to visited because 
+                # it may contain other stops we can go to from this bus
                 if bus in visited_buses:
                     continue
                 visited_buses.add(bus)
@@ -25,8 +25,8 @@ class Solution:
                 for next_stop in routes[bus]:
                     if next_stop in visited_stops:
                         continue
+                    
                     visited_stops.add(next_stop)
-                    
-                    queue.append((next_stop, buses_taken + 1))
-                    
+                    queue.append((next_stop, total_buses + 1))
+
         return -1
