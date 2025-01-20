2fe35dd3cff28430b856729accb75c315aa7de28
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2025-01-20 15:08:50 -0500

    Update Minimum Cost to Make at Least One Valid Path in a Grid.py

--- Minimum Cost to Make at Least One Valid Path in a Grid.py
+++ Minimum Cost to Make at Least One Valid Path in a Grid.py
@@ -26,6 +26,50 @@ class Solution:
         distances = [[inf for _ in range(COLS)] for _ in range(ROWS)]
         distances[0][0] = 0
 
+        queue = deque([(0, 0)])
+        while queue:
+            x, y = queue.popleft()
+
+            for _, (offset_x, offset_y) in DIRECTIONS.items():
+                next_x = x + offset_x
+                next_y = y + offset_y
+                cost = 1 if need_modify_to_reach(x, y, next_x, next_y) else 0
+
+                if is_valid_cell(next_x, next_y) and distances[x][y] + cost < distances[next_x][next_y]:
+                    distances[next_x][next_y] = distances[x][y] + cost
+                    if cost == 1:
+                        queue.append((next_x, next_y))
+                    else:
+                        queue.appendleft((next_x, next_y))
+
+        return distances[-1][-1]
+------------------------------------------------------------------------------------------------
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:        
+        def is_valid_cell(x, y):
+            return 0 <= x < ROWS and 0 <= y < COLS
+        
+        def need_modify_to_reach(x1, y1, x2, y2):
+            d = grid[x1][y1]
+            offset_x, offset_y = DIRECTIONS[d]
+            return (
+                x1 + offset_x != x2
+                or
+                y1 + offset_y != y2
+            )
+                
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        DIRECTIONS = {
+            1: (0, 1),
+            2: (0, -1),
+            3: (1, 0),
+            4: (-1, 0)
+        }
+
+        distances = [[inf for _ in range(COLS)] for _ in range(ROWS)]
+        distances[0][0] = 0
+
         heap = [(0, 0, 0)]
         while heap:
             distance, x, y = heappop(heap)
