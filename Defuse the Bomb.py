e8780cabc78cb8f7e27b1e308478edff57653fe0
Gabriel Z <58453421+gabzn@users.noreply.github.com>
2025-07-02 00:35:15 -0400

    Create Defuse the Bomb.py

---
+++ Defuse the Bomb.py
@@ -0,0 +1,38 @@
+https://leetcode.com/problems/defuse-the-bomb/description/
+
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        N = len(code)
+        TOTAL = sum(code)
+        
+        res = [0] * N
+
+        if k == 0:
+            return res
+        
+        is_reversed = False
+        if k < 0:
+            is_reversed = True
+            code.reverse()
+            k = abs(k)
+
+        window_sum = 0
+        right = 1
+        for _ in range(k):
+            window_sum += code[right]
+            right = (right + 1) % N
+
+        for i, num in enumerate(code):
+            # Update the result
+            res[i] = window_sum
+
+            # Remove the leftmost num from the window
+            window_sum -= code[(right - k) % N]
+
+            # Add rightmost num to add the window
+            window_sum += code[right % N]
+
+            # Move right pointer
+            right = (right + 1) % N
+        
+        return list(reversed(res)) if is_reversed else res
