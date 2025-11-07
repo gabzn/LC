2bb7b456e0aaea336aeef92bc657c50e26903107
gabz__n <58453421+gabzn@users.noreply.github.com>
2025-11-06 21:42:12 -0500

    Update Sliding Subarray Beauty.py

--- Sliding Subarray Beauty.py
+++ Sliding Subarray Beauty.py
@@ -1,7 +1,35 @@
 https://leetcode.com/problems/sliding-subarray-beauty/
 
-from sortedcontainers import SortedList
+class Solution:
+    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+        N = len(nums)
+        
+        res = [0] * (N - k + 1)
+        buckets = [0] * 101
 
+        for i, num in enumerate(nums):
+            buckets[num] += 1
+
+            left = i + 1 - k
+            if left < 0:
+                continue
+
+            count = x
+            # Only look at the buckets that hold negative number
+            for j in range(-50, 0):
+                # buckets[j] = how many j's we have at bucket[j]
+                # buckets[-23] = how many -23 we have at bucket[-23]
+                # If count becomes negative after subtracting the count in a bucket, 
+                # we know the number in the bucket is the answer.
+                count -= buckets[j]
+                if count <= 0:
+                    res[left] = j
+                    break
+
+            buckets[nums[left]] -= 1
+
+        return res
+____________________________________________________________________________________
 class Solution:
     def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
         res = []
@@ -21,46 +49,5 @@ class Solution:
                 res.append(0)
             
             sorted_list.remove(nums[left])
-
-        return res
-------------------------------------------------------------------------------
-class Solution:
-    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
-        N = len(nums)
-        window = defaultdict(int)
-        res = []
-        negative_count = 0
-        left = right = 0
-
-        while right < N:
-            # If the window size > k, move left 1 unit to the right
-            if right - left + 1 > k:
-                window[nums[left]] -= 1
-                if window[nums[left]] == 0:
-                    del window[nums[left]]
-
-                if nums[left] < 0:
-                    negative_count -= 1
-                left += 1                
-
-            # Add the current num to the window
-            num = nums[right]
-            if num < 0:
-                negative_count += 1
-            window[num] += 1
-            
-            # If we have at least window size, add the xth smallest
-            if right >= k - 1:
-                if negative_count < x:
-                    res.append(0)
-                else:
-                    cnt = 0
-                    for n, c in sorted(window.items()):
-                        cnt += c
-                        if cnt >= x:
-                            res.append(n)
-                            break
-
-            right += 1
 
         return res
