8bd35091fb1570f9d71366197e8cb67249ffd161
gabz__n <58453421+gabzn@users.noreply.github.com>
2025-11-07 23:10:47 -0500

    Update Permutation in String.py

--- Permutation in String.py
+++ Permutation in String.py
@@ -3,26 +3,23 @@ https://leetcode.com/problems/find-all-anagrams-in-a-string/
   
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_len, s2_len = len(s1), len(s2)
-        if s1_len > s2_len:
+        if len(s1) > len(s2):
             return False
-        
-        s1_counter, s2_counter = collections.Counter(s1), collections.Counter()
-        l, char_count = 0, 0
-        
-        for r in range(s2_len):
-            s2_counter[s2[r]] += 1
-            char_count += 1
-            
-            if char_count == s1_len:
-                if s1_counter == s2_counter:
-                    return True
-                
-                s2_counter[s2[l]] -= 1
-                if not s1_counter[s2[l]]:
-                    del s1_counter[s2[l]]
-                    
-                l += 1
-                char_count -=1
-        
+
+        counter_1 = Counter(s1)
+        counter_2 = Counter()
+        for i, char in enumerate(s2):
+            counter_2[char] += 1
+
+            left = i + 1 - len(s1)
+            if left < 0:
+                continue
+
+            if counter_1 == counter_2:
+                return True
+
+            counter_2[s2[left]] -= 1
+            if counter_2[s2[left]] == 0:
+                del counter_2[s2[left]]
+
         return False
