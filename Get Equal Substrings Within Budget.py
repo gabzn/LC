66205aa2209e1c7f50f5374de0b27314a01b0b51
gabz__n <58453421+gabzn@users.noreply.github.com>
2025-11-09 22:01:08 -0500

    Update Get Equal Substrings Within Budget.py

--- Get Equal Substrings Within Budget.py
+++ Get Equal Substrings Within Budget.py
@@ -2,6 +2,26 @@ https://leetcode.com/problems/get-equal-substrings-within-budget/
 
 class Solution:
     def equalSubstring(self, s: str, t: str, max_cost: int) -> int:
+        _len = len(s)
+        costs = [abs(ord(s[i]) - ord(t[i])) for i in range(_len)]
+
+        window_sum = 0
+        left = 0
+        res = 0
+        
+        for right, cost in enumerate(costs):
+            window_sum += cost
+
+            while window_sum > max_cost:
+                window_sum -= costs[left]
+                left += 1
+            
+            res = max(right - left + 1, res)
+            
+        return res
+______________________________________________________________________________________
+class Solution:
+    def equalSubstring(self, s: str, t: str, max_cost: int) -> int:
         N = len(s)
         
         costs = [0] * N
