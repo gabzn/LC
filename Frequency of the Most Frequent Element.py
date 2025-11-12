ad78fbed7e61034e070efed7806550a595599c0b
gabz__n <58453421+gabzn@users.noreply.github.com>
2025-11-12 00:47:06 -0500

    Update Frequency of the Most Frequent Element.py

--- Frequency of the Most Frequent Element.py
+++ Frequency of the Most Frequent Element.py
@@ -1,22 +1,21 @@
 https://leetcode.com/problems/frequency-of-the-most-frequent-element/
   
 class Solution:
-    def maxFrequency(self, nums: List[int], k: int) -> int:        
+    def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()
-        
-        max_freq = 1
         window_sum = 0
-        l, r = 0, 0   
-        
-        while r < len(nums):    
-            window_sum += nums[r]
-            
-            # nums[r] * (r-l+1) gives us the total sum if we were to change every number to [r]
-            while nums[r] * (r-l+1) > window_sum + k :
-                window_sum -= nums[l]
-                l += 1
-            
-            max_freq = max(r-l+1, max_freq)    
-            r += 1
-             
-        return max_freq
+        left = 0
+        res = 1
+
+        for right, num in enumerate(nums):
+            window_sum += num
+
+            # num * (right - left + 1) = total sum if we change all nums from [left, right] to num
+            # If total sum - window_sum > k, that means we don't have enough operations.
+            while num * (right - left + 1) - window_sum > k:
+                window_sum -= nums[left]
+                left += 1
+
+            res = max(res, right - left + 1)
+
+        return res
