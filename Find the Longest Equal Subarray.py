65b5fefbc63c9f8de2ff92b0752d46d45374bc51
gabz__n <58453421+gabzn@users.noreply.github.com>
2025-11-14 00:13:20 -0500

    Update Find the Longest Equal Subarray.py

--- Find the Longest Equal Subarray.py
+++ Find the Longest Equal Subarray.py
@@ -2,6 +2,28 @@ https://leetcode.com/problems/find-the-longest-equal-subarray/
 
 class Solution:
     def longestEqualSubarray(self, nums: List[int], k: int) -> int:
+        counter = defaultdict(int)
+        res = 1
+        cur_longest = 1
+        left = 0
+
+        for right, num in enumerate(nums):
+            counter[num] += 1
+
+            # Only move the left when num's frequency is higher 
+            # and we don't have enough k to delete other nums that are not num.
+            while counter[num] > cur_longest and (right - left + 1) - counter[num] > k:
+                counter[nums[left]] -= 1
+                left += 1
+
+            # After making the window smaller, we check if num is still the longest.
+            res = max(res, counter[num])
+            cur_longest = max(cur_longest, res)
+
+        return res
+___________________________________________________________________
+class Solution:
+    def longestEqualSubarray(self, nums: List[int], k: int) -> int:
         res = 0
         
         num_indices = defaultdict(list)
