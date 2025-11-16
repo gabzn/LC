71584db3ed4337593a0df4ae15be5d70e0823287
gabz__n <58453421+gabzn@users.noreply.github.com>
2025-11-15 21:09:38 -0500

    Update Minimum Size Subarray Sum.py

--- Minimum Size Subarray Sum.py
+++ Minimum Size Subarray Sum.py
@@ -2,38 +2,16 @@ https://leetcode.com/problems/minimum-size-subarray-sum/
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        res = math.inf
-        l, running_sum = 0, 0
-        
-        for r in range(len(nums)):
-            running_sum += nums[r]
-            
-            while running_sum >= target:
-                res = min(res, r - l + 1)
-                running_sum -= nums[l]
-                l += 1
-            
-        return res if res != math.inf else 0
----------------------------------------------------------------------------------------------------------------------------------------------------
-class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        LEN = len(nums)
-        min_size = math.inf
-        l, r, running_sum = 0, 0, 0
-        
-        # Pretty standard sliding window problem.
-        # Two pointers from the beginning. 
-        # If the window sum is >= target, update the size 
-        # chop off the leftmost val from the sum
-        # move right to its right
-        while r < LEN:
-            running_sum += nums[r]
-            
-            while running_sum >= target:
-                min_size = min(min_size, r - l + 1)
-                running_sum -= nums[l]
-                l += 1
-        
-            r += 1
-             
-        return 0 if min_size == math.inf else min_size 
+        window_sum = 0
+        left = 0
+        res = inf
+
+        for right, num in enumerate(nums):
+            window_sum += num
+
+            while window_sum >= target:
+                res = min(res, right - left + 1)
+                window_sum -= nums[left]
+                left += 1
+
+        return res if res != inf else 0
