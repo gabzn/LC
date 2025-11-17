033ccba9f19e916f5d4c32843b7697b8034df362
gabz__n <58453421+gabzn@users.noreply.github.com>
2025-11-16 23:34:28 -0500

    Update Replace the Substring for Balanced String.py

--- Replace the Substring for Balanced String.py
+++ Replace the Substring for Balanced String.py
@@ -3,6 +3,35 @@ https://www.youtube.com/watch?v=XSGa1lP9vD8
 
 class Solution:
     def balancedString(self, s: str) -> int:
+        N = len(s)
+        balance_criteria = N // 4
+
+        total_counter = Counter(s)
+        need = { 
+            char: max(total_counter[char] - balance_criteria, 0)
+            for char in "QWER" 
+        }
+        if all(count == 0 for _, count in need.items()):
+            return 0
+
+        window_counter = Counter()
+        res = N
+        left = 0
+
+        for right, char in enumerate(s):
+            window_counter[char] += 1
+
+            # Check whether inside window covers all required excess
+            # Shrink the window whenever we have enough chars in the window to replace
+            while all(window_counter[char] >= need[char] for char in "QWER"):
+                res = min(res, right - left + 1)
+                window_counter[s[left]] -= 1
+                left += 1
+
+        return res
+___________________________________________________________________
+class Solution:
+    def balancedString(self, s: str) -> int:
         def is_size_mid_good(window_size):
             window_counter = Counter()
             for i in range(N):
